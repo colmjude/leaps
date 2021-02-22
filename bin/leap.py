2642b2486937453c1050bcf63b39c01bb5393fe5
Colm Britton <colmjude@gmail.com>
2021-02-22 23:00:53 +0000

    create full ics calendar for the 10 leaps

--- bin/leap.py
+++ bin/leap.py
@@ -1,4 +1,37 @@
+import datetime
+
+from ics import Calendar
+
 from bin.cal import ics_event
+from bin.file import read_json_file
+from bin.dates import add_weeks
+
+
+def leap_dates(due_date, leap_start):
+    start_date = add_weeks(due_date, leap_start)
+    return start_date
+
+
+def create_calendar():
+    return Calendar()
+
+
+class Leaps:
+    DUE_DATE = datetime.datetime.strptime("2021-01-02", "%Y-%m-%d")
+
+    def __init__(self, path_to_data="leaps.json"):
+        self.data_file = path_to_data
+        self.data = self.read_csv()
+
+    def read_csv(self):
+        return read_json_file(self.data_file)
+
+    def calendar(self):
+        cal = create_calendar()
+        for n, data in self.data.items():
+            leap = Leap(n, leap_dates(self.DUE_DATE, data["start"]), data["name"])
+            cal.events.add(leap.event())
+        return cal
 
 
 class Leap:
@@ -8,7 +41,7 @@ class Leap:
         self.name = name
 
     def event(self):
-        print(ics_event(self.name, self.start))
+        return ics_event(self.name, self.start)
 
     def print(self):
         print(f"Leap {self.num} - {self.name}")
--- leaps.py
+++ leaps.py
@@ -1,20 +1,14 @@
 import datetime
 
-from bin.dates import add_weeks
 from bin.file import read_json_file
 
-from bin.leap import Leap
+from bin.leap import Leaps, Leap
 
 DUE_DATE = datetime.datetime.strptime("2021-01-02", "%Y-%m-%d")
 
+leaps = Leaps("leaps.json")
+print(leaps.calendar())
 
-def leap_dates(due_date, leap_start):
-    start_date = add_weeks(due_date, leap_start)
-    return start_date
-
-
-leaps_data = read_json_file("leaps.json")
-
-for n, data in leaps_data.items():
-    leap = Leap(n, leap_dates(DUE_DATE, data["start"]), data["name"])
-    leap.event()
+# for n, data in leaps_data.items():
+#     leap = Leap(n, leap_dates(DUE_DATE, data["start"]), data["name"])
+#     leap.event()
